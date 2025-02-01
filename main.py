75a82f700f3dc61ac4c65d1a297fc33df3ff8166
zahra <z.durandish52@gmail.com>
2025-02-01 14:52:00 -0800

    some updates added

--- main.py
+++ main.py
@@ -29,38 +29,46 @@ def compare(u_score, c_score):
     elif u_score > 21:
         return "You went over, you lose!"
     elif c_score > 21:
-        return "The opponent went over, you lose!"
+        return "The opponent went over, you win!"
     elif u_score > c_score:
         return "You win!"
     else:
         return "You lose!"
 
+def play_game():
+    user_cards = []
+    computer_cards = []
+    computer_score = -1
+    user_score = -1
+    game_over = False
 
-user_cards = []
-computer_cards = []
-computer_score = -1
-user_score = -1
-game_over = False
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
 
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
+    while not game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"Your cards: {user_cards}, Current score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
 
-while not game_over:
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
-    print(f"Your cards: {user_cards}, Current score: {user_score}")
-    print(f"Computer's first card: {computer_cards[0]}")
-
-    if user_score == 0 or computer_score == 0 or user_score > 21:
-        game_over = True
-    else:
-        should_continue = input("Type 'y' to draw another card. Type 'n' to pass: ").lower()
-        if should_continue == "y":
-            user_cards.append(deal_card())
-        else:
+        if user_score == 0 or computer_score == 0 or user_score > 21:
             game_over = True
+        else:
+            should_continue = input("Type 'y' to draw another card. Type 'n' to pass: ").lower()
+            if should_continue == "y":
+                user_cards.append(deal_card())
+            else:
+                game_over = True
 
-while computer_score < 17 and computer_score != 0:
-    computer_cards.append(deal_card())
-    computer_score = calculate_score(computer_cards)
+    while computer_score < 17 and computer_score != 0:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"Your final hand: {user_cards}, final score: {user_score}")
+    print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
+
+restart = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+while restart == "y":
+    play_game()
