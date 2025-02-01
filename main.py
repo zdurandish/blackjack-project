7b03f9638132808a793a0de27fe9740acd03e165
zahra <z.durandish52@gmail.com>
2025-02-01 14:28:59 -0800

    compare function defined

--- main.py
+++ main.py
@@ -18,18 +18,49 @@ def calculate_score (list):
     return score
 
 
+def compare(u_score, c_score):
+    """Takes both the user and the computer scores and compares them."""
+    if u_score == c_score:
+        return "It's a draw!"
+    elif c_score == 0:
+        return "The opponent had a blackjack. You lose!"
+    elif u_score == 0:
+        return "You had a blackjack. You win!"
+    elif u_score > 21:
+        return "You went over, you lose!"
+    elif c_score > 21:
+        return "The opponent went over, you lose!"
+    elif u_score > c_score:
+        return "You win!"
+    else:
+        return "You lose!"
+
+
 user_cards = []
 computer_cards = []
+computer_score = -1
+user_score = -1
 game_over = False
 
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
-print(f"Your cards: {user_cards}, Current score: {user_score}")
-print(f"Computer's first card: {computer_cards[0]}")
+while not game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards}, Current score: {user_score}")
+    print(f"Computer's first card: {computer_cards[0]}")
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    game_over = True
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        game_over = True
+    else:
+        should_continue = input("Type 'y' to draw another card. Type 'n' to pass: ").lower()
+        if should_continue == "y":
+            user_cards.append(deal_card())
+        else:
+            game_over = True
+
+while computer_score < 17 and computer_score != 0:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
