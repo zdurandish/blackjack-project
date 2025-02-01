0dbd33b34b82a40e6837163e1a8d35ea78df251f
zahra <z.durandish52@gmail.com>
2025-02-01 13:45:16 -0800

    game over conditions defined

--- main.py
+++ main.py
@@ -17,3 +17,19 @@ def calculate_score (list):
         list.append(1)
     return score
 
+
+user_cards = []
+computer_cards = []
+game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"Your cards: {user_cards}, Current score: {user_score}")
+print(f"Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    game_over = True
